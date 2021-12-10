d8ea0406c4e01a765fa98cdf16fe571edd921355
Erich Lima <erich.timoteo@santander.com.br>
2021-12-10 18:23:05 -0300

    teste

--- src/main/java/br/com/santander/example/saga/events/TransactionConsumer2.java
+++ src/main/java/br/com/santander/example/saga/events/TransactionConsumer2.java
@@ -1,21 +1,9 @@
 package br.com.santander.example.saga.events;
 
-import br.com.santander.example.saga.events.compensation.TransactionProducerCompensation;
-import br.com.santander.example.saga.model.Card;
-import br.com.santander.example.saga.model.Transaction;
-import br.com.santander.example.saga.service.TransactionService;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
-import org.eclipse.microprofile.reactive.messaging.Outgoing;
 import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-import javax.json.bind.Jsonb;
-import javax.json.bind.JsonbBuilder;
-
-import static br.com.santander.example.saga.model.Card.BALANCE_UPDATED;
-import static br.com.santander.example.saga.model.Transaction.*;
-
 
 @ApplicationScoped
 public class TransactionConsumer2 {
